7cb83e3948cc39ca8285fef7c2f15bc607c201f0
Pronin Mike <mr.mi@mail.ru>
2023-06-30 19:47:28 +0400

    HW4

--- src/main/java/org/example/lesson4/HomeWork/Task1.java
+++ src/main/java/org/example/lesson4/HomeWork/Task1.java
@@ -1,5 +1,7 @@
 package org.example.lesson4.HomeWork;
 
+import java.util.*;
+
 public class Task1 {
     public static void main(String[] args) {
 //        1)Вывести список на экран в перевернутом виде (без массивов и ArrayList)
@@ -7,6 +9,34 @@ public class Task1 {
 //        1 -> 2->3->4
 //        Вывод:
 //        4->3->2->1
+        LinkedList<Integer> inLinkedList = new LinkedList<>();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите размер списка: ");
+        int n = sc.nextInt();
+        sc.close();
+        Random random = new Random();
+        for (int i = 0; i < n; i++) {
+            inLinkedList.add(random.nextInt(10));
+        }
+        System.out.println("Исходный список 1:" + inLinkedList);
+        System.out.println("Перевернутый список: ");
+        Collections.reverse(inLinkedList);
 
+        System.out.println("inLinkedList = " + inLinkedList);
+
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        System.out.println("Исходный список 2:" + list);
+        List<Integer> reverse = reverseList(list);
+        System.out.println("reverseList = " + reverse);
+    }
+
+    private static List<Integer> reverseList(List<Integer> list) {
+        List<Integer> reverse = new ArrayList<>(list.size());
+
+        new LinkedList<>(list)
+                .descendingIterator()
+                .forEachRemaining(reverse::add);
+
+        return reverse;
     }
 }
--- src/main/java/org/example/lesson4/HomeWork/Task2.java
+++ src/main/java/org/example/lesson4/HomeWork/Task2.java
@@ -1,5 +1,8 @@
 package org.example.lesson4.HomeWork;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Task2 {
     public static void main(String[] args) {
 //        Учитывая строку s, содержащую только символы '(', ')', '{', и '}', определите, допустима ли входная строка.'['']'
@@ -15,6 +18,27 @@ public class Task2 {
 //        Ввод: s = "()"
 //        Вывод: правда
 
-
+        String s = "())()"; // false
+        String startBrack = "({[";
+        String endBrack = ")}]";
+        boolean result = true;
+        Deque<Character> deque = new ArrayDeque<>();
+        char[] chars = s.toCharArray();
+        for (char ch : chars) {
+            if (startBrack.indexOf(ch) >= 0) {
+                deque.offerFirst(ch);
+            } else {
+                int index = endBrack.indexOf(ch);
+                if (index >= 0) {
+                    if (deque.size() == 0 || deque.pollFirst() != startBrack.charAt(index)) {
+                        result = false;
+                        break;
+                    }
+                }
+            }
+            if (!result)
+                break;
+        }
+        System.out.println(result);
     }
 }
