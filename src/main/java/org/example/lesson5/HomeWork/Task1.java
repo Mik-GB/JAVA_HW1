d2361772e5e7e8e5e79cf42f2a26563d4fa34a54
Pronin Mike <mr.mi@mail.ru>
2023-07-04 20:55:53 +0400

    HW5

--- src/main/java/org/example/lesson5/HomeWork/Task1.java
+++ src/main/java/org/example/lesson5/HomeWork/Task1.java
@@ -1,4 +1,34 @@
 package org.example.lesson5.HomeWork;
 
+import java.util.*;
+
 public class Task1 {
+    public static void main(String[] args) {
+//    1) Подсчитать количество искомого слова, через map (наполнением значение, где искомое слово будет являться ключом),
+//    вносить все слова не нужно
+//    Пример:
+//    Россия идет вперед всей планеты. Планета — это не Россия.
+//        Запрос: Россия
+//    Ответ: Россия - 2
+//    toLoverCase().
+//            (Усложнение - игнорировать пунктуацию)
+
+        Map<String, Integer> mapCh = new HashMap<>();
+        String str = "Россия идет вперед всей планеты. Планета — это не Россия.";
+        String[] text = str.toLowerCase().split(" ");
+        System.out.println(Arrays.toString(text));
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите искомое слово: ");
+        String word = sc.nextLine();
+        String wordLow = word.toLowerCase();
+        for (int i = 0; i < text.length; i++) {
+            if (text[i] == wordLow){
+                mapCh.putIfAbsent(str.charAt(i), 0);
+                mapCh.put(str.charAt(i), mapCh.get(str.charAt(i)) + 1);
+            }
+        }
+        for (Map.Entry<String, Integer> entity : mapCh.entrySet()) {
+            System.out.println(entity.getKey() + " встретился в тексте: " + entity.getValue() + " раз");
+        }
+    }
 }
--- src/main/java/org/example/lesson5/HomeWork/Task2.java
+++ src/main/java/org/example/lesson5/HomeWork/Task2.java
@@ -1,13 +1,75 @@
 package org.example.lesson5.HomeWork;
 
-public class Task2 {
-//    1) Подсчитать количество искомого слова, через map (наполнением значение, где искомое слово будет являться ключом),
-//    вносить все слова не нужно
-//    Пример:
-//    Россия идет вперед всей планеты. Планета — это не Россия.
-//            Запрос: Россия
-//    Ответ: Россия - 2
-//    toLoverCase().
-//            (Усложнение - игнорировать пунктуацию)
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
+public class Task2 {
+//2) Пусть дан список сотрудников:
+//    Иван Иванов
+//    Светлана Петрова
+//    Кристина Белова
+//    Анна Мусина
+//    Анна Крутова
+//    Иван Юрин
+//    Петр Лыков
+//    Павел Чернов
+//    Петр Чернышов
+//    Мария Федорова
+//    Марина Светлова
+//    Мария Савина
+//    Мария Рыкова
+//    Марина Лугова
+//    Анна Владимирова
+//    Иван Мечников
+//    Петр Петин
+//    Иван Ежов
+//    Написать программу, которая найдёт и выведет повторяющиеся имена с количеством повторений.
+//    Отсортировать по убыванию популярности.
+public static void main(String[] args) {
+    Map<String, Integer> nameMap = new HashMap<>();
+    String employees = "Иван Иванов " +
+            "Светлана Петрова " +
+            "Кристина Белова " +
+            "Анна Мусина " +
+            "Анна Крутова " +
+            "Иван Юрин " +
+            "Петр Лыков " +
+            "Павел Чернов " +
+            "Петр Чернышов " +
+            "Мария Федорова " +
+            "Марина Светлова " +
+            "Мария Савина " +
+            "Мария Рыкова " +
+            "Марина Лугова " +
+            "Анна Владимирова " +
+            "Иван Мечников " +
+            "Петр Петин " +
+            "Иван Ежов ";
+    String[] listOfFio = employees.split(" ");
+    for (int i = 0; i < listOfFio.length; i += 2) {
+        if (nameMap.containsKey(listOfFio[i])) {
+            nameMap.replace(listOfFio[i], nameMap.get(listOfFio[i]) + 1);
+        } else {
+            nameMap.put(listOfFio[i], 1);
+        }
+    }
+    System.out.println(nameMap);
+    Map<String, Integer> sortedNameMap = new LinkedHashMap<>();
+    int max = 1;
+    for (int value : nameMap.values()) {
+        if (value > max) {
+            max = value;
+        }
+    }
+    for (int i = max; i > 0; i--) {
+        for (Map.Entry<String, Integer> entry : nameMap.entrySet()) {
+            String key = entry.getKey();
+            if (nameMap.get(key) == i) {
+                sortedNameMap.put(key, nameMap.get(key));
+            }
+        }
+    }
+    System.out.println(sortedNameMap);
+}
 }
